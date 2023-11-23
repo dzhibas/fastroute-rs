7caa8d7eae601059e57f2646ce5949e57f4cb4a5
dzhibas <nikolajus@gmail.com>
2023-11-23 21:58:05 +0100

    Bulk route test

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 use regex::Regex;
 
 fn main() {
-   let re = Regex::new(r"^/demo/[^/]+$").unwrap();
-   let message = "/demo/as";
-
-   dbg!(re.is_match(message));
+   let re = Regex::new(r"(?x)^(?:
+                (?<r1>/foo/([^/]+)/(\d+))
+              | (?<r2>/bar/(?<id>\d+))
+              | (?<r3>/foi/([^/]+))
+            )$").unwrap();
+   let route: &str = "/foi/33";
+   let cap = re.captures(route).unwrap();
+   dbg!(&cap);
+   for c in cap.iter().skip(1) {
+      if c.is_some() {
+         let b = c.unwrap();
+         dbg!(b);
+      }
+   }
+   let matches: Vec<_> = re.find_iter(route).map(|m| m.as_str()).collect();
+   dbg!(matches);
 }
